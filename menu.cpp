1e09f271a28033127de640d0c8af0d37ade5e2a2
Massimiliano Prandi <maxallep@gmail.com>
2024-03-13 11:23:41 +0100

    commented menu

--- menu.cpp
+++ menu.cpp
@@ -1,6 +1,13 @@
 #include <ncurses.h>
 #include "menu.hpp"
 
+// Constructor
+// initializes the height, width, start_y, and start_x
+// 
+// @param h = height 
+// @param w = width
+// @param y = start_y
+// @param x = start_x
 Menu::Menu(int h, int w, int y, int x){
     height = h;
     width = w;
@@ -8,6 +15,10 @@ Menu::Menu(int h, int w, int y, int x){
     start_x = x;
 };
 
+// Menu method: show
+// shows the menu
+//
+// @return void
 void Menu::show(){
     initscr();
 
@@ -17,6 +28,7 @@ void Menu::show(){
     char border = ' ';
 
     wborder(window, border, border, border, border, border, border, border, border);
+    
     mvprintw(start_y+1, (start_x + width - 6)/2, "Tetris");
     wrefresh(window);
 
@@ -24,11 +36,17 @@ void Menu::show(){
 
     mvprintw(start_y + 5, start_x + 1, "Enter your name: ");
     getstr(name);
+
+    clear();
+    mvprintw(start_y+1, (start_x + width - 6)/2, "Tetris");
     
-    mvprintw(start_y + 7, start_x + 1, "Hello, %s", name);
+    mvprintw(start_y + 5, start_x + 1, "Hello, %s", name);
+
+    mvprintw(start_y + 5, start_x + 1, "Press any key to play", name);
+
     wrefresh(window);
 
-    int close = getch();
+    getch();
 
     endwin();
 };
